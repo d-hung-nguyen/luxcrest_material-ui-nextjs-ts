f4f54b8b16e1efbc1b36a0fefe00bb43423e961f
Duy Hung Nguyen <nd.hung@me.com>
2024-05-18 11:22:17 +0200

    chore: Remove unused components and update layout and logo components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,8 +11,6 @@ import AutoAwesomeRoundedIcon from "@mui/icons-material/AutoAwesomeRounded";
 import AppAppBar from "@/components/AppAppBar";
 import Hero from "@/components/Hero";
 import LogoCollection from "@/components/LogoCollection";
-import Highlights from "@/components/Highlights";
-import Pricing from "@/components/Pricing";
 import Features from "@/components/Features";
 import Testimonials from "@/components/Testimonials";
 import FAQ from "@/components/FAQ";
